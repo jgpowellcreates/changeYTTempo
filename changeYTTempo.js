bef8236eb4703122e756da3e96c36e74238d6013
Jacob Powell <jacobgarrisonpowell@gmail.com>
2022-01-17 14:57:02 -0500

    Rearranges files for clearer naming convention. changeYTTempo.js is now standalone function

--- changeYTTempo.js
+++ changeYTTempo.js
@@ -1,36 +1,25 @@
-// Removing the function to see how Extension behaves w/ automatically run behavior
-console.log("changeYTTemplate script runs EACH TIME I open the window");
+function changeTempoTo(input) {
+    let endingTempo = input;
+    let startingTempo = 0;
+    let grabTitle = document.querySelector('.title > yt-formatted-string').innerHTML;
+    let grabDesc = document.querySelector('#description > yt-formatted-string.content').children;
+    let bpmRegex = /\d{2,3}(?= ?bpm)/i;
+    let titleResult = grabTitle.match(bpmRegex);
+    let descResult;
+    if (!titleResult) {
+        for (lines of grabDesc) {
+            let descBpm = lines.innerHTML.match(bpmRegex);
+            if (descBpm) {descResult = descBpm}
+        }
+    }
+    if (titleResult) {
+        startingTempo = titleResult[0]
+    } else if (descResult) {
+        startingTempo = descResult[0]
+    }
 
-let submitBtn = document.querySelector("button#submitChange");
-submitBtn.addEventListener('click', sayTempo);
-
-function sayTempo(e) {
-    e.preventDefault();
-    console.log("Clicked button",)
+    let altPlayback = (endingTempo/startingTempo).toString().substring(0,6);
+    document.querySelector('video').playbackRate = altPlayback;
+    let userFeedback = console.log("Starting tempo:",startingTempo); console.log("You wanted to play at:", endingTempo); console.log(`Speed Adjusted to ${altPlayback} the original speed.`)
+    return userFeedback
 }
-
-// function changeTempoTo(input) {
-//     let endingTempo = input;
-//     let startingTempo = 0;
-//     let grabTitle = document.querySelector('.title > yt-formatted-string').innerHTML;
-//     let grabDesc = document.querySelector('#description > yt-formatted-string.content').children;
-//     let bpmRegex = /\d{2,3}(?= ?bpm)/i;
-//     let titleResult = grabTitle.match(bpmRegex);
-//     let descResult;
-//     if (!titleResult) {
-//         for (lines of grabDesc) {
-//             let descBpm = lines.innerHTML.match(bpmRegex);
-//             if (descBpm) {descResult = descBpm}
-//         }
-//     }
-//     if (titleResult) {
-//         startingTempo = titleResult[0]
-//     } else if (descResult) {
-//         startingTempo = descResult[0]
-//     }
-
-//     let altPlayback = (endingTempo/startingTempo).toString().substring(0,6);
-//     document.querySelector('video').playbackRate = altPlayback;
-//     let userFeedback = console.log("Starting tempo:",startingTempo); console.log("You wanted to play at:", endingTempo); console.log(`Speed Adjusted to ${altPlayback} the original speed.`)
-//     return userFeedback
-// }
--- popup.js
+++ popup.js
@@ -1,24 +1,40 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// Removing the function to see how Extension behaves w/ automatically run behavior
+console.log("changeYTTemplate script runs EACH TIME I open the window");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+let submitBtn = document.querySelector("button#submitChange");
+let startTempoInput = document.querySelector('input#startTempoInput');
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+submitBtn.addEventListener('click', sayTempo);
+
+function sayTempo(e) {
+    e.preventDefault();
+    console.log("Clicked button",)
+    console.log("Found Start Tempo?", startTempoInput)
+}
+
+//Chrome Set Up Information Below
+
+// // Initialize button with user's preferred color
+// let changeColor = document.getElementById("changeColor");
+
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
+
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: setPageBackgroundColor,
+//     });
+//   });
   
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+//   // The body of this function will be executed as a content script inside the
+//   // current page
+//   function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//       document.body.style.backgroundColor = color;
+//     });
+//   }
